e1c5b2c49e61196de7ea6bf7c269f3812c0a6000
gelinnnnn <ntlinh.qb30@gmail.com>
2024-09-18 12:55:36 +0700

    PRACTICE_05.sql

--- PRACTICE_05.sql
+++ PRACTICE_05.sql
@@ -1,3 +1,4 @@
+--PRACTICE_05
 --Bài 1
 select b.continent
 , round(avg(a.population),0)
@@ -98,4 +99,69 @@ left join page_likes as b
 on a.page_id = b.page_id
 where b.user_id is null
 order by a.page_id
+
+--Mid-course Test
+--Bài 1
+  select distinct replacement_cost
+from film
+order by replacement_cost
+--Bài 2
+  select 
+count(case when replacement_cost between 9.99 and 19.99 then replacement_cost end) as low 
+, count(case when replacement_cost between 20 and 24.99 then replacement_cost end) as medium 
+, count(case when replacement_cost between 25 and 29.99 then replacement_cost end) as high 
+from film
+--Bài 3
+  select a.title
+, a.length
+, c.name as category_name
+from film as a
+left join film_category as b on a.film_id = b.film_id
+left join category as c on c.category_id = b.category_id
+where c.name in ('Drama', 'Sports')
+order by a.length desc
+--Bài 4
+  select count(a.title) as so_luong_phim
+, c.name
+from film as a
+left join film_category as b on a.film_id = b.film_id
+left join category as c on c.category_id = b.category_id
+group by c.name
+order by so_luong_phim desc
+--Bài 5
+  select a.first_name
+, a.last_name
+, count(b.film_id) as so_luong_phim
+from actor as a 
+left join film_actor as b
+on a.actor_id = b.actor_id
+group by a.first_name, a.last_name
+order by so_luong_phim desc
+--Bài 6
+  select count(*)
+from address as a
+left join customer as b
+on a.address_id = b.address_id
+where b.address_id is null
+--Bài 7
+select 
+a.city
+, sum(d.amount)
+from city as a
+join address as b on a.city_id = b.city_id
+join customer as c on b.address_id = c.address_id
+join payment as d on c.customer_id = d.customer_id
+group by a.city
+order by sum(amount) desc
+--Bài 8
+select concat(e.country, ', ', a.city)
+, sum(d.amount) as doanh_thu
+from city as a
+join address as b on a.city_id = b.city_id
+join customer as c on b.address_id = c.address_id
+join payment as d on c.customer_id = d.customer_id
+join country as e on e.country_id = a.country_id
+group by concat(e.country, ', ', a.city)
+order by doanh_thu desc
+
 
